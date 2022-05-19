292eb7b3b1bc8982439448393c9cb6583610eedd
bradwray <bradwray@gmail.com>
2022-05-19 11:18:33 -0400

    added more

--- debates.js
+++
@@ -1,6 +0,0 @@
-const debates = [
-  "Are there more doors or wheels in the world?",
-  "Are there more rocks or hair folicles?",
-  "Are there more trees or leaves?",
-  "",
-];
---
+++ questions.js
@@ -0,0 +1,42 @@
+const questions = [
+  `Are there more doors or wheels in the world?`,
+  `Are there more rocks or hair folicles?`,
+  `Are there more trees or leaves?`,
+  `What is the most important part of a broom?`,
+  `Would you rather be born smart or become smart?.`,
+  `Who is more important, you or your neighbor?`,
+  `What was your best ever Halloween costume?`,
+  `Would you rather have four 3-week breaks in the school year or keep summer the way it is? Why?`,
+  `Would you rather be 5 years older or 5 years younger? Why?`,
+  `If you could live forever inside a computer* would you do it?
+      *with full access to the internet, social media, chat and email`,
+  `You have an invincibility suit. You can be transported through time into any of the following scenarios for 15 minutes and survive to come back. Which do you choose and why?
+  * T-Rex nest
+  * WWII battlefield
+  * Surface of the sun
+  * Roman gladiator arena
+  * other idea?
+`,
+  `Would you rather be a pigeon or a squirrel? Why?`,
+  `Would you rather have a special secret credit card for the rest of your life that you don't have to pay off but is limited to $250/day in purchases or instead be given $1 million dollars once. Why?`,
+  `Would you rather be living as you are 10 years ago or 10 years in the future?
+Why?`,
+  `Would you rather be a famous person or a famous animal? Why?`,
+  `Would you rather be a hammer or a nail? Why?`,
+  `Would you rather live in a place where it snows a little every day or where it rains a little every day? Why?`,
+  `Would you rather be stuck in the 1960s or 1990s? Why?`,
+  `Would you rather be President of the U.S. or Batman / Batwoman? Why?`,
+  `If you could be someone else (anyone from the past or present) who would you be, when, where, why?`,
+  `Would you rather lose your sense of taste or lose your sense of touch? Why?`,
+  `Would you rather have a black eye or broken toe? Why?`,
+  `If you could move your birthday then when would you move it to and why?`,
+  `Would you rather be an Emergency Room doctor or an airline pilot? Why?`,
+  `Would you rather win an Oscar for a movie or a gold medal in the Olympics? Why?`,
+  `Would you rather be a ninja or Navy Seal? Why?`,
+  `
+What is your favorite TV show from your childhood? Why?`,
+  `If you could be somewhere else in the world right now, where would it be? Why?`,
+  `If you could start a business what would it be? Why?`,
+  `What has been the best thing (album or song) you listened to in the past few months? Why?`,
+  `What has been the best game you played in the past year?`,
+];
